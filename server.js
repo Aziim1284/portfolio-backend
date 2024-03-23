7b48ee4e82315bf0ab12af46a635cc26bd2101de
azeembhatti <aziimbhatti613@gmail.com>
2024-03-23 14:33:27 +0530

    added cors

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors()); // Use CORS middleware
+app.use(cors({
+  origin: 'https://portfolio-backend-vert-delta.vercel.app'
+}));
+
 app.use(express.json());
 
 // Connect to MongoDB
